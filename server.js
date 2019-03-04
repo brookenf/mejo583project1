b1ed3164bd7102c0bc715134760e9ff0ff894dda
Glitch (brookenf-mejo583project1) <none>
2019-03-04 20:14:52 +0000

    🛳👦 Checkpoint ./server.js:3109110/18

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 // where your node app starts
 
 // init project
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -15,6 +15,8 @@ app.use(express.static('public'));
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
