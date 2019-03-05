d3282626bea9d267223bf63fd26147b233498f98
Glitch (brookenf-mejo583project1) <none>
2019-03-05 01:41:48 +0000

    🐚👾 Checkpoint ./public/client.js:3109110/148 ./server.js:3109110/872

--- public/client.js
+++ public/client.js
@@ -107,15 +107,12 @@ document.addEventListener("DOMContentLoaded", () => {
       .addTo(controller);
   }
   
-  var jsonData = [];
   
   // Marvel API data
   fetch('/characters').then(resp => resp.json()).then((data) => {
     console.group('%cResponse from /characters', 'color: #4B9CD3; font-size: large');
     console.log(data);
     console.groupEnd();  
-    
-    jsonData.push(JSON.stringify(data));
     
     var html = '';
     var char = document.getElementById('marvel-characters');
@@ -137,7 +134,6 @@ document.addEventListener("DOMContentLoaded", () => {
   
   });
   
-  console.log(jsonData);
 
   // end of Document.addEventlistener
 });
--- server.js
+++ server.js
@@ -39,6 +39,18 @@ app.get('/characters', function (request, response) {
     
     response.send(data.data);
     
+    // write to a file 
+    var fs = require("fs");
+    var characters10 = data.data;
+
+    fs.writeFile("./charaters.json", JSON.stringify(characters10), (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        };
+        console.log("File has been created");
+    });
+    
     }, function(err) {
       console.error(err);
     });    
